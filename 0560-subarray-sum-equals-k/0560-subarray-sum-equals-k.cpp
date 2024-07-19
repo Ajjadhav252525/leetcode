55698cc8d5e2449bde7d1788fa906cdb84ec1c0f
JadhavAjay <119773485+Ajjadhav252525@users.noreply.github.com>
2024-07-19 17:34:40 +0530

    Time: 109 ms (29.46%), Space: 53.9 MB (10.03%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        map<int ,int>mp ;
+        mp[0]=1 ;
+        int sum=0 ;
+        int cnt=0 ;
+        for(int i=0 ;i<nums.size() ;i++)
+        {
+            sum+=nums[i] ;
+            cnt+=mp[sum-k] ;
+            mp[sum]++ ;
+            
+        }
+        return cnt ;
+    }
+};
