ea9ae1c9e36c693a891f8c25b79dc07f1cc2994e
JadhavAjay <119773485+Ajjadhav252525@users.noreply.github.com>
2024-07-20 16:45:38 +0530

    Time: 10 ms (71.40%), Space: 27.5 MB (15.67%) - LeetHub

---
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    void createmapping(vector<int> &in , int n , unordered_map<int ,int>&mp )
+    {
+        for(int i=0 ;i<n ;i++)
+        {
+            mp[in[i]] =i;
+        }
+    }
+    TreeNode * construct(vector<int> &p, vector<int> &i , int &index, int n , int start , int end ,unordered_map<int ,int>&mp )
+    {
+        if(index <0 || start > end)
+        {
+            return NULL ;
+        }
+        TreeNode * root= new TreeNode(p[index--]);
+        int pos = mp[root->val] ;
+        root->right= construct(p, i , index, n , pos+1  ,end , mp) ;
+        root->left = construct(p ,i , index, n, start, pos-1 , mp);
+        return root ;
+    }
+public:
+    TreeNode *buildTree(vector<int> &i, vector<int> &p) 
+    {
+        unordered_map<int ,int>mp ;
+        createmapping( i , p.size() , mp);
+        int preindex=p.size()-1;
+        TreeNode * root =construct(p , i ,preindex , p.size() ,0 , p.size()-1 , mp);
+        return root ;
+    }
+};
