dbc036213bd011e3a7a9e18175be6099a737ee91
JadhavAjay <119773485+Ajjadhav252525@users.noreply.github.com>
2024-07-21 00:04:37 +0530

    Time: 161 ms (50.00%), Space: 32.1 MB (50.00%) - LeetHub

---
+++ 3223-minimum-length-of-string-after-operations/3223-minimum-length-of-string-after-operations.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int minimumLength(string s) {
+        unordered_map<char ,int>mp ;
+        for(auto it:s)
+        {
+            mp[it]++ ;
+        }
+        int ans= 0;
+        for(auto it:mp)
+        {
+            if(it.second<3)
+            {
+                ans+=it.second ;
+            }
+            else
+            {
+                if(it.second%2==0)
+                {
+                    ans+=2 ;
+                }
+                else
+                {
+                    ans++ ;
+                }
+            }
+        }return ans ;
+    }
+};
