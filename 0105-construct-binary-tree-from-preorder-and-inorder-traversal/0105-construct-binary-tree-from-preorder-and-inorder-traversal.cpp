334a6c42e969c91c4e622b0d42326a6be395d384
JadhavAjay <119773485+Ajjadhav252525@users.noreply.github.com>
2024-07-20 16:37:14 +0530

    Time: 16 ms (38.07%), Space: 27.4 MB (18.79%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -10,25 +10,32 @@
  * };
  */
 class Solution {
-public:
-    TreeNode *buildTree(vector<int> &preorder, vector<int> &inorder) {
-    return create(preorder, inorder, 0, preorder.size() - 1, 0, inorder.size() - 1);
-}
-
-TreeNode* create(vector<int>& preorder, vector<int>& inorder, int ps, int pe, int is, int ie){
-    if(ps > pe){
-        return nullptr;
-    }
-    TreeNode* node = new TreeNode(preorder[ps]);
-    int pos;
-    for(int i = is; i <= ie; i++){
-        if(inorder[i] == node->val){
-            pos = i;
-            break;
+    void createmapping(vector<int> &in , int n , unordered_map<int ,int>&mp )
+    {
+        for(int i=0 ;i<n ;i++)
+        {
+            mp[in[i]] =i;
         }
     }
-    node->left = create(preorder, inorder, ps + 1, ps + pos - is, is, pos - 1);
-    node->right = create(preorder, inorder, pe - ie + pos + 1, pe, pos + 1, ie);
-    return node;
-}
+    TreeNode * construct(vector<int> &p, vector<int> &i , int &index, int n , int start , int end ,unordered_map<int ,int>&mp )
+    {
+        if(index >=n || start > end)
+        {
+            return NULL ;
+        }
+        TreeNode * root= new TreeNode(p[index++]);
+        int pos = mp[root->val] ;
+        root->left = construct(p ,i , index, n, start, pos-1 , mp);
+        root->right= construct(p, i , index, n , pos+1  ,end , mp) ;
+        return root ;
+    }
+public:
+    TreeNode *buildTree(vector<int> &p, vector<int> &i) 
+    {
+        unordered_map<int ,int>mp ;
+        createmapping( i , p.size() , mp);
+        int preindex=0 ;
+        TreeNode * root =construct(p , i ,preindex , p.size() ,0 , p.size()-1 , mp);
+        return root ;
+    }
 };
