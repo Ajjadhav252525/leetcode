f91f1731f2621333ed9bf7db25d18b63beb7c2af
JadhavAjay <119773485+Ajjadhav252525@users.noreply.github.com>
2024-07-20 17:28:58 +0530

    Time: 15 ms (39.40%), Space: 21.2 MB (14.89%) - LeetHub

--- 0275-h-index-ii/0275-h-index-ii.cpp
+++ 0275-h-index-ii/0275-h-index-ii.cpp
@@ -1,12 +1,18 @@
 class Solution {
 public:
     int hIndex(vector<int>& c) {
-        int ans =0,low = 0, n = c.size(), high =1000;
-        for(int i=0 ; i<n ; i++)
+        int ans =0,low = 0, n = c.size(), high =c.size()-1;
+        while(low<=high)
         {
-            if(c[i] >= n-i)
+            int mid =low+(high -low)/2 ;
+            if(c[mid] >= n-mid )
             {
-                ans =max(ans ,n-i);
+                ans = max(ans, n-mid);
+                            high =mid-1 ;
+
+            }
+            else
+            {    low = mid+1 ;
             }
         }
         return ans ;
